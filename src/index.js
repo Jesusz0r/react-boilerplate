074cfe01da6fd16f480e37310ee6bbab7f99e13c
Jesus Mendoza <jesusreveron1990@gmail.com>
2019-07-21 21:30:07 -0500

    Initial react configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<App />, app);
