ab646da920a935b37aa76f1c4cbd4cf4f951e2ed
Jesus Mendoza <jesusreveron1990@gmail.com>
2019-07-21 22:11:52 -0500

    Added react-hot-loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world awa!</h1>;
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <p>Bye, world!</p>
+      </div>
+    );
   }
 }
 
-export default App;
+export default hot(module)(App);
